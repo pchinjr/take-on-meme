a6da2734633e4ee462daa612ffb9493d36062377
Paul Chin Jr. <pchinjr@gmail.com>
2024-04-29 19:49:53 +0000

    fix: comment hack for now

--- src/data/comment_api.ts
+++ src/data/comment_api.ts
@@ -15,25 +15,7 @@ const __dirname = path.dirname(__filename);
 export class CommentAPI {
   private commentsFilePath = path.resolve(__dirname, "comments.json");
 
-  private comments: Comment[] = [
-    {
-      id: "1",
-      name: "Maggie",
-      comment: "Such a great song!",
-    },
-    {
-      id: "2",
-      name: "Jonas",
-      comment: "I've always loved this track.",
-    },
-    {
-      id: "3",
-      name: "Claire",
-      comment: "This brings back memories.",
-    },
-  ];
-
-  async fetchComments(): Promise<Comment[]> {
+  fetchComments(): Promise<Comment[]> {
     const commentsData = fs.readFileSync(this.commentsFilePath, "utf-8");
     return JSON.parse(commentsData);
   }
